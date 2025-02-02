9dd45aaeb23609a33df6b31cb6d6ddd77cf5358b
Adrijan Parmakovski <adrijan.parmakovski@gmail.com>
2025-02-02 13:38:08 +0100

    register sites in admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+admin.site.register(models.Room)
+admin.site.register(models.Furniture)
+admin.site.register(models.Storage)
+admin.site.register(models.InventoryObject)
