946e17042d7c3e395944ac8cd858ac058d1062b0
Adrijan Parmakovski <adrijan.parmakovski@gmail.com>
2025-02-02 12:39:20 +0100

    rename the app and project

--- api/apps.py
+++ api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class InventoryAppConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'inventory_app'
+    name = 'api'
