f24a52e197b4646e7bda24fb112ee6c7b901e8cb
Adrijan Parmakovski <adrijan.parmakovski@gmail.com>
2025-02-02 13:38:25 +0100

    fix some

--- api/models.py
+++ api/models.py
@@ -15,6 +15,7 @@ class Furniture(models.Model):
     name = models.CharField(max_length=100, blank=False)
     slug = models.CharField(max_length=100, unique=True, blank=False)
     origin = models.CharField(max_length=100)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, blank=False, null=True)
 
     def __str__(self):
         return f"{self.name}"
@@ -24,12 +25,14 @@ class Storage(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=100, choices=choices.STORAGE_TYPES, blank=False)
     slug = models.CharField(max_length=100, unique=True, blank=False)
+    furniture = models.ForeignKey(
+        Furniture, on_delete=models.CASCADE, blank=True, null=True
+    )
 
     def __str__(self):
         return f"{self.name}"
 
 
-# Create your models here.
 class InventoryObject(models.Model):
     # name
     name = models.CharField(max_length=100, blank=False)
@@ -40,6 +43,9 @@ class InventoryObject(models.Model):
     origin = models.CharField(max_length=100)
     to_remove = models.BooleanField(null=True)
     comment = models.TextField(null=True)
+    storage = models.ForeignKey(
+        Storage, on_delete=models.CASCADE, blank=True, null=True
+    )
 
-
-# Create your models here.
+    def __str__(self):
+        return f"{self.name}"
