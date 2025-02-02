134b5abf758ce2b0ef07a5baa08959210afeb759
Adrijan Parmakovski <adrijan.parmakovski@gmail.com>
2025-02-02 12:43:45 +0100

    add a file with the field choices

---
+++ api/choices.py
@@ -0,0 +1,18 @@
+INVENTORY_OBJECT_TYPES = (
+    (DOCUMENT, "Document"),
+    (BOOK, "Book"),
+    (BOARD_GAME, "Board Game"),
+    (CLOTHES, "Clothes"),
+    (SOUVENIR, "Souvenir"),
+    (MEDICINE, "Medicine"),
+    (COSMETICS, "Cosmetics"),
+    (ELECTRONICS, "Electronics"),
+)
+
+STORAGE_TYPES = (
+    (BOX, "Box"),
+    (FOLDER, "Folder"),
+    (DRAWER, "Drawer"),
+    (SHELF, "Shelf"),
+    (BAG, "Bag"),
+)
